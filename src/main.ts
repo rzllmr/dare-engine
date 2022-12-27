5b340dc41bb2e84cad90ff9a0f841d8114e4c113
rzllmr <30299783+rzllmr@users.noreply.github.com>
2022-12-27 11:26:26 +0100

    Define scene in separate file

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { Application } from 'pixi.js'
+import { Scene } from './scene'; // This is the import statement
+
+const app = new Application({
+    view: document.getElementById("pixi-canvas") as HTMLCanvasElement,
+    resolution: window.devicePixelRatio || 1,
+    autoDensity: true,
+    backgroundColor: 0x6495ed,
+    width: 640,
+    height: 480
+});
+
+// pass in the screen size to avoid "asking up"
+const scene = new Scene(app.screen.width, app.screen.height);
+
+app.stage.addChild(scene);
