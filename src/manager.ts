c47ca8c67437728e676e1694dbed87917755c180
rzllmr <30299783+rzllmr@users.noreply.github.com>
2023-01-14 23:31:42 +0100

    Declare function interface in property style

--- src/manager.ts
+++ src/manager.ts
@@ -69,5 +69,5 @@ export class Manager {
 // This could have a lot more generic functions that you force all your scenes to have. Update is just an example.
 // Also, this could be in its own file...
 export interface IScene extends DisplayObject {
-    update(framesPassed: number): void;
+    update: (framesPassed: number) => void;
 }
