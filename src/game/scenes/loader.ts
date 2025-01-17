40c16ade5abce9a0204a40e95ca8dad7878dbeaf
rzllmr <30299783+rzllmr@users.noreply.github.com>
2025-01-18 00:53:19 +0100

    Fade splash-screen to game scene

--- src/game/scenes/loader.ts
+++ src/game/scenes/loader.ts
@@ -5,15 +5,18 @@ import { GameScene } from './game';
 import { yamlParser } from '../../extensions/yamlParser';
 import { htmlParser } from '../../extensions/htmlParser';
 import { inkParser } from '../../extensions/inkParser';
+import { Animation } from '../../engine/animation';
 
 export class LoaderScene extends Container implements IScene {
     private readonly splashScreen: HTMLDivElement;
+    private readonly loaderBar: HTMLDivElement;
     private readonly loaderBarFill: HTMLDivElement;
     
     constructor() {
         super();
 
         this.splashScreen = document.querySelector('#splash-screen') as HTMLDivElement;
+        this.loaderBar = document.querySelector('.progress-bar') as HTMLDivElement;
         this.loaderBarFill = document.querySelector('.progress-bar > div') as HTMLDivElement;
     }
 
@@ -31,6 +34,7 @@ export class LoaderScene extends Container implements IScene {
 
         const bundleIds = manifest.bundles.map((bundle: any) => bundle.name);
 
+        this.loaderBar.style.visibility = 'visible';
         await Assets.loadBundle(bundleIds, this.showProgress.bind(this));
     }
 
@@ -46,9 +50,12 @@ export class LoaderScene extends Container implements IScene {
         const gameScene = new GameScene();
         console.log(`${gameScene.gameName} v${version}`);
 
-        this.splashScreen.style.display = 'none';
-
-        manager.changeScene(gameScene);
+        manager.changeScene(gameScene).then(() => {
+            this.loaderBar.style.visibility = 'hidden';
+            Animation.fade(this.splashScreen, 200).run().then(() => {
+                this.splashScreen.style.display = 'none';
+            });
+        });
     }
 
     public input(position: Point, button?: string): void {}
