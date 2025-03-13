f485c8c245b6f0fd55d4eddc827e20de5fd48e76
rzllmr <30299783+rzllmr@users.noreply.github.com>
2025-03-13 13:07:54 +0100

    Add story spec to tell component

--- src/game/components/tell.ts
+++ src/game/components/tell.ts
@@ -1,22 +1,35 @@
 import { ComponentSpecs } from 'engine/specs';
 import { Tile } from 'game/entities/tile';
-import { log } from 'game/proxies/log';
+import { dialog } from 'game/proxies/dialog';
 import { addComponent } from './registry';
 import { Action } from './types';
 
 export class Tell extends Action {
     constructor(specs: ComponentSpecs) {
-        super(specs, 'text');
+        super(specs, 'line');
     }
 
     public override async act(subject: Tile): Promise<void> {
         if (subject.name !== 'player') return;
 
-        log.tell(this.text);
+        if (this.story != undefined) {
+            const story = this.story.split('.');
+            dialog.tellStory(story[0], story[1]);
+        } else {
+            dialog.tell(this.line);
+        }
     }
 
-    public get text(): string {
-        return this.specs.get('text', '');
+    public get line(): string {
+        return this.specs.get('line', '');
+    }
+
+    public get story(): string | undefined {
+        return this.specs.get('story');
+    }
+
+    public get once(): boolean {
+        return this.specs.get('once', false);
     }
 }
 
